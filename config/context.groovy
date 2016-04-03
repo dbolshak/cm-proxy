e3a33306dd09788dfafa5bf6ededdcb41acb8b09
Denis Bolshakov <denis.bolshakov@megafon.ru>
2016-04-03 23:25:06 +0300

    TODO: need to handle NPE

--- config/context.groovy
+++ config/context.groovy
@@ -1,19 +1,22 @@
 package com.dbolshak.cm.proxy
 
 import com.cloudera.api.ClouderaManagerClientBuilder
-import com.cloudera.api.DataView
-import com.cloudera.api.model.ApiClusterList
+//import com.cloudera.api.DataView
+//import com.cloudera.api.model.ApiClusterList
 
 beans {
     clouderaManagerClientBuilder(ClouderaManagerClientBuilder)
 
-    rootResourceHolder(RootResourceHolder, clouderaManagerClientBuilder,
-            '192.168.1.11', 'admin', 'admin'
+    rootResourceHolder(
+            RootResourceHolder,
+            clouderaManagerClientBuilder,
+            '192.168.1.11',
+            'admin',
+            'admin'
     )
-
-    apiClusterList(ApiClusterList) {
-        clusters = clouderaManagerClientBuilderAdapter
-                .rootResource
-                .readClusters(DataView.EXPORT)
-    }
+//    apiClusterList(ApiClusterList) {
+//        clusters = rootResourceHolder
+//                .rootResource
+//                .readClusters(DataView.EXPORT)
+//    }
 }
